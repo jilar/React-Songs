df44709e960b5237b40cb4e3dc6a994d8f9fde78
Jeffrey Ilar <jilar@petzent.com>
2019-10-17 17:08:53 -0700

    created action,reducers, and redux store. Connected SongList component to redux store. Added SongDetail component.

--- src/index.js
+++ src/index.js
@@ -1,7 +1,13 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import App from './Components/App'
+import {Provider} from 'react-redux'
+import {createStore} from 'redux'
+import App from './components/App'
+import reducers from './reducers'
 
 ReactDOM.render(
-  <App />, document.getElementById('root')
+  <Provider store={createStore(reducers)}>
+    <App />
+  </Provider>,
+  document.getElementById('root')
 );
