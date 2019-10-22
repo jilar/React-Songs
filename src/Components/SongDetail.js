33d8b6a3f81c7c3db845e0403801c57dead5aa14
Jeffrey Ilar <jilar@petzent.com>
2019-10-22 12:53:31 -0700

    Displayed Song List, wired selectSong Action to selct button. Wired selectedSong state data to SongDetail Component

--- src/Components/SongDetail.js
+++ src/Components/SongDetail.js
@@ -1,3 +1,31 @@
 import React from 'react'
+import {connect} from 'react-redux';
 
-export default SongDetail;
+const SongDetail =({selectedSong})=>{
+  if (!selectedSong){
+    return(
+      <div>
+        <h4>Select a Song</h4>
+      </div>
+    );
+  };
+    return (
+      <div>
+        <h4>Details For:</h4>
+        <div>
+          {selectedSong.title}
+        </div>
+        <div>
+          Duration : {selectedSong.duration}
+        </div>
+      </div>
+    );
+};
+
+const mapStateToProps=(state)=>{
+  //selectedSong is now a prop for SongDetail
+  return {selectedSong:state.selectedSong};
+}
+
+
+export default connect(mapStateToProps)(SongDetail);
